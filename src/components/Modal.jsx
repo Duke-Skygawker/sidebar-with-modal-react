4cda7cd35e5c6323836495ca2f91259f39cd7e8d
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-14 19:06:00 +0100

    fixed modal background

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,12 +4,14 @@ const Modal = () => {
   const { closeCross, showModal, setShowModal } = useGlobalContext();
   return (
     showModal && (
-      <div className="modal">
-        <div className="modal-content">
-          <button className="close-btn" onClick={() => setShowModal(false)}>
-            {closeCross}
-          </button>
-          <p>Modal content</p>
+      <div className="modal-background">
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close-btn" onClick={() => setShowModal(false)}>
+              {closeCross}
+            </button>
+            <p>Modal content</p>
+          </div>
         </div>
       </div>
     )
